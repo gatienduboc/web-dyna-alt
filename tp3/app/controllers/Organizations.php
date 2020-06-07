289e8d1e5f68270924b62c563dcb8e220c055b49
Gatien DUBOC <gatien.duboc@gmail.com>
2020-06-07 16:11:01 +0200

    TP3: 2.3

--- tp3/app/controllers/Organizations.php
+++ tp3/app/controllers/Organizations.php
@@ -18,11 +18,17 @@ class Organizations extends \Ubiquity\controllers\ControllerBase{
     }
 
     /**
-     * @get("{idOrga}","name"=>"orgas-display")
-     **/
-    public function display($idOrga){
-        $orga=DAO::getById(Organization::class, $idOrga, ['users','groupes']);
-        $this->loadView("Organizations/display.html",["orga"=>$orga]);
+    * @get("{idOrga}/{idGroupe}","name"=>"orgas-display")
+    **/    
+    public function display($idOrga,$idGroupe=null){
+        $orga=DAO::getById(Organization::class, $idOrga,['users','groupes']);
+        $users=$this->users($orga->getUsers());
+        $this->jquery->renderView("Organizations/display.html",["orga"=>$orga,"users"=>$users]);
+    }
+    
+    protected function users($users=null){
+        $title="Tous les utilisateurs";
+        return $this->loadView("Organizations/users.html",compact("users","title"),true);
     }
 
 }
